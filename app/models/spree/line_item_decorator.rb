a51f8a29e91296175950c32087817177b0e19351
Heath Anderson <handerson@blacksnowmedia.com>
2015-06-29 21:10:34 -0400

    use tax_address

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,10 +1,8 @@
 Spree::LineItem.class_eval do
   def tax_cloud_cache_key
     key = "Spree::LineItem #{id}: #{quantity}x<#{variant.cache_key}>@#{price}#{currency}"
-    if order.ship_address
-      key << "shipped_to<#{order.ship_address.try(:cache_key)}>"
-    elsif order.billing_address
-      key << "billed_to<#{order.bill_address.try(:cache_key)}>"
+    if order.tax_address
+      key << "tax_address<#{order.tax_address.try(:cache_key)}>"
     end
   end
 end
